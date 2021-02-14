829bfe97ccbfd9c1e77b1753ca96152e28fd2ca4
Jarred Evans <jarrede99@gmail.com>
2021-02-14 01:13:51 -0700

    fixed the time logging for new task creation

--- testmain.cpp
+++ testmain.cpp
@@ -1,6 +1,6 @@
 #include "appOpener.hpp"
 #include "tasks.hpp"
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <chrono>
 #include <string>
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace chrono;
 
-time_t parseTime(string time24, TimePoint_s ref1, TimePoint ref2);
+time_t parseTime(string time24);
 Task* timeCheck(vector<Task> tdl);
 
 
@@ -17,32 +17,33 @@ int main(){
     TimePoint_s referenceTime1 = systemClock::now();
     TimePoint referenceTime2 = Clock::now();
     
-    char firefox[] = "\"C:\\Program Files\\Mozilla Firefox\\firefox.exe\"";
+    char firefox[] = "\"..\\..\\..\\..\\..\\Program Files\\Mozilla Firefox\\firefox.exe\"";
     char google[] = "google.com";
-    // char zoom[] = "..\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe";
     char zoom[] = "C:\\Users\\new22\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe";
 
-    Task *openGoogle = new Task("open google", parseTime("22:15", referenceTime1, referenceTime2), firefox, google);
+    // Task *openGoogle = new Task("open google", parseTime("22:15"), firefox, google);
 
-    Task *zoomTask = new Task("zoom lecture !", parseTime("22:15", referenceTime1, referenceTime2), zoom);
+    // Task *zoomTask = new Task("zoom lecture !", parseTime("22:15"), zoom);
 
     runProgram(firefox, google);
 
     runProgram(zoom);
 
-    time_t ten;
+    time_t timeTest;
     time_t currTime;
     time(&currTime);
-    
-    ten = parseTime("23:00", referenceTime1, referenceTime2);
-    printf("the parsed eleven o'clock would be %s", ctime(&ten));
+
+    string eleven45 = "23:45";
+    cout << "passing " << eleven45 << " \n";
+    timeTest = parseTime(eleven45);
+    printf("the parsed eleven thirty would be %s", ctime(&timeTest));
     printf("it is currently %s", ctime(&currTime));
 
-    char MSWord[] = "\"C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE\"";
-    char file[] = "\"D:\\UBC Accessibility.docx\"";
+    // char MSWord[] = "\"C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE\"";
+    // char file[] = "\"D:\\UBC Accessibility.docx\"";
 
 
-    runProgram(MSWord, file);
+    // runProgram(MSWord, file);
 
     return 0;
 }
@@ -65,11 +66,21 @@ Task* timeCheck(vector<Task> tdl){
     return &(*it);
 }
 
-time_t parseTime(string time24, TimePoint_s ref1, TimePoint ref2){
+time_t parseTime(string time24){
     size_t idx = 0;
-    Clock::duration hrs = hours(stoi(time24, &idx, 10));
-    Clock::duration mins = minutes(stoi(time24, &idx, 10));
-    Clock::duration secs = duration_cast<seconds>(hrs) + duration_cast<seconds>(mins);
-    TimePoint desiredTime(secs);
-    return  systemClock::to_time_t(systemClock::now() + duration_cast<systemClock::duration>(desiredTime - Clock::now()));;;
+    int hrs = (stoi(time24, &idx, 10));
+    idx = 3;
+    int mins = (stoi(time24.substr(idx) , &idx, 10));
+    time_t now;
+
+    time(&now);
+    struct tm* tm = localtime(&now);
+    tm->tm_hour += hrs - tm->tm_hour;
+    tm->tm_min += mins - tm->tm_min;
+    tm->tm_sec = 0;
+    return mktime(tm);
 }
+
+
+  
+
